dacc346bd3162a3deb05a7a2e7b3a40b4c5da91e
Matthew Nelson <kb0mgq@gmail.com>
2017-07-10 11:55:28 -0500

    minor update

--- 3-Firmware/snappyImages/LTA_Kill_Board.py
+++ 3-Firmware/snappyImages/LTA_Kill_Board.py
@@ -25,9 +25,12 @@ Portal_Response = 0
 @setHook(HOOK_STARTUP)
 def startupEvent():
     global LTA_Alive
+    global BOARD_ADDR
     # Set the LTA Board to respond to multicast group 2 and forward it on
     saveNvParam(5, 0x0002)
     saveNvParam(6, 0x0002)
+    
+    BOARD_ADDR = str(byteToHex(ord(localAddr()[0]))) + ":" + str(byteToHex(ord(localAddr()[1]))) + ":" + str(byteToHex(ord(localAddr()[2])))
     
     # Set RF266 Pin 20 to be an output pin
     setPinDir(LATCH_CTRL_PIN, True)
@@ -64,6 +67,15 @@ def kill_LTA():
     LTA_Alive = LTA_BOARD_KILLED
     if Portal_Response == 1:
         rpc(PORTAL_ADDR, "logEvent", loadNvParam(8) + " has been killed")
+        
+def low_batt():
+    global LTA_Alive
+    global Portal_Response
+    writePin(LATCH_CTRL_PIN, True)
+    adc = readAdc(BATTERY_PIN)
+    LTA_Alive = LTA_BOARD_LOW_BATT
+    if Portal_Response == 0:
+        rpc(PORTAL_ADDR, "logEvent", loadNvParam(8) + " Disabling board, battery is Low!  ADC: " + str(adc))
 
 #TODO Update
 # Enable the motors and received by setting the latch control pin low
@@ -72,7 +84,7 @@ def restart_LTA():
     global Portal_Response
     writePin(LATCH_CTRL_PIN, False)
     LTA_Alive = LTA_BOARD_RESURRECTED
-    if Portal_Response == 1:
+    if Portal_Response == 0:
         rpc(PORTAL_ADDR, "logEvent", loadNvParam(8) + " has been resurrected")
 
 # Return the current battery voltage as a number between 0-1023 every second
@@ -81,16 +93,48 @@ def status_check():
     global Portal_Response
     global LTA_Alive
     adc = readAdc(BATTERY_PIN)
-    if Portal_Response == 0:
+    #if Portal_Response == 0:
+     #   rpc(PORTAL_ADDR, "ping")
+      #  rpc(PORTAL_ADDR, "logEvent", loadNvParam(8) + " ONLINE Batt Level: " + str(adc))
+       # rpc(PORTAL_ADDR, "logEvent", "LTA_Add_Board", loadNvParam(8), LTA_Alive)
+    
+    if LTA_Alive == 1:
         rpc(PORTAL_ADDR, "ping")
-        rpc(PORTAL_ADDR, "logEvent", loadNvParam(8) + " was already active")
-        rpc(PORTAL_ADDR, "LTA_Add_Board", loadNvParam(8), LTA_Alive, adc)
+        rpc(PORTAL_ADDR, "logEvent", loadNvParam(8) + " ONLINE, ADDR: " + BOARD_ADDR + " ADC: " + str(adc))
+    
+    if LTA_Alive == 2:
+        rpc(PORTAL_ADDR, "ping")
+        rpc(PORTAL_ADDR, "logEvent", loadNvParam(8) + " OFFLINE!!!  " + str(adc))
+        
+    if LTA_Alive == 3:
+        rpc(PORTAL_ADDR, "ping")
+        rpc(PORTAL_ADDR, "logEvent", loadNvParam(8) + " IT'S ALIVE!!!")
+        
+    if LTA_Alive == 4:
+        rpc(PORTAL_ADDR, "ping")
+        rpc(PORTAL_ADDR, "logEvent", loadNvParam(8) + " LOW BATTERY!!!")
 
     # Kill the LTA if the battery voltage is below 5.8V
     if (adc <= BATTERY_LOW_FLIGHT) and (LTA_Alive == 1 or LTA_Alive == 3):
-        kill_LTA()
+        low_batt()
 
 def update_info():
     global LTA_Alive
     adc = readAdc(BATTERY_PIN)
     return str(loadNvParam(8) + "_" + str(adc) + "_" + str(LTA_Alive))
+
+def hexNibble(nibble):
+    '''Convert a numeric nibble 0x0-0xF to its ASCII string representation "0"-"F"'''
+    hexStr = "0123456789ABCDEF"
+    return hexStr[nibble & 0xF]
+
+def printHex(byte):
+    '''print a byte in hex - input is an integer, not a string'''
+    print hexNibble(byte >> 4),
+    print hexNibble(byte),         # no trailing CR/LF
+    
+def byteToHex(byte):
+    '''print a byte in hex - input is an integer, not a string'''
+    upper = hexNibble(byte >> 4)
+    lower = hexNibble(byte)
+    return upper + lower
--- 3-Firmware/snappyImages/LTA_Kill_Button.py
+++ 3-Firmware/snappyImages/LTA_Kill_Button.py
@@ -1,25 +1,23 @@
-from pinWakeupATmega128RFA1 import *
-
 # Tell the script where to find the Portal to log the data
 PORTAL_ADDR = '\x00\x00\x01'
 
 # Pin the switch is connected to
-BUTTON_PIN = 0
+BUTTON_PIN = 9
 
 @setHook(HOOK_STARTUP)
 def startupEvent():
     # Set SM200 Pin F1 to be an input pin
     setPinDir(BUTTON_PIN, False)
-    setPinPullup(BUTTON_PIN, False)
+    #setPinPullup(BUTTON_PIN, False)
     monitorPin(BUTTON_PIN, True)
     rpc(PORTAL_ADDR, "logEvent", loadNvParam(8) + " has been turned on")
     
 @setHook(HOOK_GPIN)
-def button_pressed(pin, isSet):
-    if pin == BUTTON_PIN:
+def button_pressed(pinNum, isSet):
+   if pinNum == BUTTON_PIN:
         rpc(PORTAL_ADDR, "logEvent", loadNvParam(8) + " is killing LTAs")
         mcastRpc(0x0002, 3, 'kill_LTA')
         
 @setHook(HOOK_1S)
-def second_timer():
-    rpc(PORTAL_ADDR, "LTA_Check_In")
+def status_check():
+    rpc(PORTAL_ADDR, "logEvent", loadNvParam(8) + " checking in...")
